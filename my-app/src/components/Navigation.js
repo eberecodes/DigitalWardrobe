24b321f0e5b08918d3bfed517179e043de79624e
danielleebere <danielleebereanukem@gmail.com>
2024-03-21 17:17:07 +0000

    #1 Updated the navigation bar so that there is a dropdown menu, changed the logo

--- my-app/src/components/Navigation.js
+++ my-app/src/components/Navigation.js
@@ -1,29 +1,35 @@
 import React from 'react';
-import { Navbar, Nav, Form, Button } from 'react-bootstrap';
+import { Navbar, Nav, Button } from 'react-bootstrap';
 import { withRouter, Link } from 'react-router-dom';
-import "./Navigation.css";
 import logo from './logo.png';
+import Container from 'react-bootstrap/Container';
+import history from '.././history';
+import "./Navigation.css";
 
-const Navigation = (props) => {
-    console.log(props);
+const Navigation = () => {
     return (
         <Navbar>
-          <Nav className="mr-auto">
-            <Nav.Link href="/">
-                   <Navbar.Brand>
-                         <img
-                            src={logo}
-                            width="70"
-                            height="90"
-                            className="d-inline-block align-top"
-                            alt="Logo"
-                         />
-                   </Navbar.Brand>
-             </Nav.Link>
-            <Nav.Link href="/AddItem">Add Item</Nav.Link>
-          </Nav>
+            <Container fluid>
+                <Navbar.Brand href="/">
+                    <img
+                        src={logo}
+                        alt="Logo"
+                        className="logo-img"
+                    />
+                </Navbar.Brand>
+                <Nav className="dropdown-container">
+                    <Button className="dropdown-button">
+                        <span className="menu-icon">&#9776;</span>
+                    </Button>
+                    <div className="dropdown-content">
+                        <Link to="/" onClick={() => history.push('/')}>Home</Link>
+                        <Link to="/AddItem" onClick={() => history.push('/AddItem')}>Add Item</Link>
+                        <Link to="/Wardrobe" onClick={() => history.push('/Wardrobe')}>View Wardrobe</Link>
+                    </div>
+                </Nav>
+            </Container>
         </Navbar>
-    )
+    );
 }
 
 export default withRouter(Navigation);
