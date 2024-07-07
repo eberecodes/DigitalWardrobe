abbc80e532c8ec1844d10e58da409b65a2686318
danielleebere <danielleebereanukem@gmail.com>
2024-07-07 20:56:59 +0100

    #1 fixed AddItem.js to also supply size to ProductDetail

--- my-app/src/AddItem.js
+++ my-app/src/AddItem.js
@@ -68,6 +68,7 @@ const AddItem = () => {
                 title={title}
                 brand={brand}
                 category={category}
+                size={size}
               />
             )}
     </div>
--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -1,5 +1,4 @@
 import React, { useContext } from "react";
-import { Button } from 'react-bootstrap';
 import history from './history';
 import ImageCarousel from './components/ImageCarousel';
 import { ImageContext } from './ImageContext';
